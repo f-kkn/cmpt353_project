bcbbd103dad65e1302258d5b26b995c07f695d41
flyte <flyte.1ne@gmail.com>
2023-11-23 00:58:08 -0600

    Added Landing/log-in page. Need to add sign-up page

---
+++ frontend/react-project/src/Landing.jsx
@@ -0,0 +1,35 @@
+import React, {useState} from "react";
+
+function Landing(){
+    const [UserName, SetUserName] = useState("");
+    const [Password, SetPassword] = useState("");
+
+
+    function checkButton(){
+        console.log(`${UserName} and ${Password}`);
+    }
+
+    return(
+        <div className="Landing-page">
+            <h1> Stack Overflow: Kind Of </h1> <br/> <br/>
+            <div className="sign-in-block">
+                <div className="sign-in-form">
+                    <input
+                        type="text"
+                        placeholder="Username"
+                        value={UserName}
+                        onChange={(data) => {SetUserName(data.target.value)}}
+                    /> <br/> <br/>
+                    <input 
+                        type="text"
+                        placeholder="Password"
+                        value={Password}
+                        onChange={(data) => {SetPassword(data.target.value)}}
+                    /> <br/> <br/>
+                    <button onClick={checkButton}> Submit </button>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Landing;
