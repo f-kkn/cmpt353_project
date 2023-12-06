c2765114fc10bb6295f09023eec55a2291c4013b
flyte <flyte.1ne@gmail.com>
2023-12-06 12:40:21 -0600

    final touches to the reports

--- backend/api/postdb.js
+++ backend/api/postdb.js
@@ -2,6 +2,6 @@ exports.create = (conn, table, postName, postData, userCreds) => {
     let addPost = `INSERT INTO ${table}(postName, postData, channel_id) VALUES('${postName}', "${postData}", ${userCreds.cid})`;
     conn.query(addPost, (err) => {
         if(err) throw err;
-        console.log(`User ${userCreds.cid} added into Posts table: ${postName} post.`);
+        console.log(` ${postName} has been added to Channel Table ${userCreds.cid}.`);
     });
 }
